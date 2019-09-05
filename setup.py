381aeea1d78d65d8c9bec3bd4122bc56656d6da7
Vladimir Stavrinov <vstavrinov@gmail.com>
2019-09-05 19:20:53 +0300

    add cli

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version='1.0.2',
     packages=find_packages("."),
     package_dir={'acestream_search': '.'},
-    entry_points={'console_scripts': ['acestream_search=acestream_search.acestream_search:main']},
+    entry_points={'console_scripts': ['acestream_search=acestream_search.acestream_search:cli']},
 )
