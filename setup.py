21d3e870e2b0ff0193f1b109effaf2d443805600
Vladimir Stavrinov <vstavrinov@gmail.com>
2019-09-05 18:36:46 +0300

    add flake8 test

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='acestream_search',
     version='1.0.2',
-    packages=['acestream_search'],
+    packages=find_packages("."),
     package_dir={'acestream_search': '.'},
     entry_points={'console_scripts': ['acestream_search=acestream_search.acestream_search:main']},
 )
