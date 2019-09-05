5a4442cff0ae137a6c3d2c5294a08d275f081548
Vladimir Stavrinov <vstavrinov@gmail.com>
2019-09-05 19:56:55 +0300

    bug fix for setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='acestream_search',
     version='1.0.2',
-    packages=find_packages("."),
+    packages=['acestream_search'],
     package_dir={'acestream_search': '.'},
     entry_points={'console_scripts': ['acestream_search=acestream_search.acestream_search:cli']},
 )
