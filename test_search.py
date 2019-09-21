ea9c08ea324fa504d2994d15183a5a3952950aaa
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2019-09-21 21:00:11 +0300

    bug fix: 'group' option 'KeyError' bug fix: tests: regex has no effect tests: add 'group' test tests: compile regex for 3 tests

--- test_search.py
+++ test_search.py
@@ -5,6 +5,9 @@ import re
 import unittest
 from . import acestream_search
 channel = 'НТВ'
+m3u_re = re.compile('#EXTINF:-1,' + channel +
+                    '.*\n.*/ace/manifest.m3u8\\?infohash=[0-9a-f]+')
+
 if sys.version_info[0] > 2:
     def u_code(string):
         return string
@@ -22,18 +25,27 @@ class TestQuery(unittest.TestCase):
     def test_query(self):
         acestream_search.args = acestream_search.get_options()
         acestream_search.args.query = channel
-        re.match('#EXTINF:-1,' + channel +
-                 '.*\n.*/ace/manifest.m3u8\\?infohash=[0-9a-f]+',
-                 probe(acestream_search.args))
+        self.assertIsNotNone(m3u_re.match(probe(acestream_search.args)))
+
+    def test_name(self):
+        acestream_search.args = acestream_search.get_options()
+        acestream_search.args.name = channel
+        self.assertIsNotNone(m3u_re.match(probe(acestream_search.args)))
+
+    def test_group(self):
+        acestream_search.args = acestream_search.get_options()
+        acestream_search.args.query = channel
+        acestream_search.args.group_by_channels = 1
+        self.assertIsNotNone(m3u_re.match(probe(acestream_search.args)))
 
     def test_epg(self):
         acestream_search.args = acestream_search.get_options()
         acestream_search.args.query = channel
         acestream_search.args.show_epg = 1
         acestream_search.args.group_by_channels = 1
-        re.match('#EXTINF:-1 tvg-id="[0-9]+",' + channel +
-                 '.*\n.*/ace/manifest.m3u8\\?infohash=[0-9a-f]+',
-                 probe(acestream_search.args))
+        self.assertIsNotNone(re.match('#EXTINF:-1 tvg-id="[0-9]+",' + channel +
+                             '.*\n.*/ace/manifest.m3u8\\?infohash=[0-9a-f]+',
+                                      probe(acestream_search.args)))
 
     def test_xml(self):
         acestream_search.args = acestream_search.get_options()
@@ -41,8 +53,8 @@ class TestQuery(unittest.TestCase):
         acestream_search.args.xml_epg = 1
         acestream_search.args.show_epg = 1
         acestream_search.args.group_by_channels = 1
-        re.match(' +<channel id="[0-9]+">\\n +<display-name lang="ru">'
-                 + channel, probe(acestream_search.args))
+        self.assertIsNotNone(re.search(' +<channel id="[0-9]+">\\n +<display-name lang="ru">'
+                             + channel, probe(acestream_search.args)))
 
     def test_json(self):
         acestream_search.args = acestream_search.get_options()
@@ -51,10 +63,3 @@ class TestQuery(unittest.TestCase):
         item = json.loads(probe(acestream_search.args), encoding='utf8')[0]
         self.assertTrue(channel in u_code(item['name']) and
                         re.match('[0-9a-f]+', item['infohash']))
-
-    def test_name(self):
-        acestream_search.args = acestream_search.get_options()
-        acestream_search.args.name = channel
-        re.match('#EXTINF:-1,' + '"' + channel + '"' +
-                 '.*\n.*/ace/manifest.m3u8\\?infohash=[0-9a-f]+',
-                 probe(acestream_search.args))
