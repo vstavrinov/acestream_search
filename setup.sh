daded751df5cb5a33eb2b4d33d9c2d28fc046bca
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2020-11-07 21:00:27 +0300

    fix acestream download

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ mkdir /opt/acestream
 cd /opt/acestream
 curl -s http://acestream.org/downloads/linux/acestream_3.1.49_debian_9.9_x86_64.tar.gz |
 tar xzf -
-"./acestreamengine --client-console --log-file acestream.log 2>&1 & cd -"
+./acestreamengine --client-console --log-file acestream.log 2>&1 & cd -
 pip install --disable-pip-version-check --upgrade pip setuptools
 pip install pytest-cov pytest-flake8 codecov
 
