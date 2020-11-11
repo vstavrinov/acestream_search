c577dab1bb4273d91a69cb30f1fe8485203754c1
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2020-11-11 18:16:23 +0300

    move wheel installation to setup step to provision install step

--- setup.sh
+++ setup.sh
@@ -11,6 +11,6 @@ ACE_VERSION="3.1.49_debian_9.9_x86_64"
 curl -s http://acestream.org/downloads/linux/acestream_${ACE_VERSION}.tar.gz |
 tar xzf -
 ./acestreamengine --client-console --log-file acestream.log 2>&1 & cd -
-pip install --disable-pip-version-check --upgrade pip setuptools
+pip install --disable-pip-version-check --upgrade pip setuptools wheel
 pip install pytest-cov pytest-flake8 codecov
 
