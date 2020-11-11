805183fa06de97afae86d8380b7f1b3b1faaa830
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2020-11-11 17:14:03 +0300

    -  expand  python matrix
    - add the dependency for pypy support

--- setup.py
+++ setup.py
@@ -26,10 +26,10 @@ setup(
                  'Operating System :: POSIX',
                  'Operating System :: Microsoft :: Windows',
                  'Operating System :: MacOS',
-                 'Programming Language :: Python :: 2.7',
-                 'Programming Language :: Python :: 3.5',
                  'Programming Language :: Python :: 3.6',
                  'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8',
+                 'Programming Language :: Python :: 3.9',
                  'Topic :: Internet :: WWW/HTTP',
                  'Topic :: Multimedia :: Video',
                  'Topic :: Utilities']
--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y --no-install-recommends python-apsw
+sudo apt-get install -y --no-install-recommends \
+    python-apsw                                 \
+    libxml2-dev                                 \
+    libxslt1-dev
 mkdir /opt/acestream
 cd /opt/acestream
-curl -s http://acestream.org/downloads/linux/acestream_3.1.49_debian_9.9_x86_64.tar.gz |
+ACE_VERSION="3.1.49_debian_9.9_x86_64"
+curl -s http://acestream.org/downloads/linux/acestream_${ACE_VERSION}.tar.gz |
 tar xzf -
 ./acestreamengine --client-console --log-file acestream.log 2>&1 & cd -
 pip install --disable-pip-version-check --upgrade pip setuptools
