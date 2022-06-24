5eac99c6f05740396cbbffa5d3db4ab21c558c98
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2022-06-24 23:08:15 +0300

    use acestream engine from docker container

--- setup.sh
+++ setup.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install -y --no-install-recommends \
-    python-apsw                                 \
-    libxml2-dev                                 \
-    libxslt1-dev                                \
-    libpython2.7                                \
-    python-pkg-resources
-mkdir /opt/acestream
-cd /opt/acestream
-ACE_VERSION="3.1.49_debian_9.9_x86_64"
-curl -s http://acestream.org/downloads/linux/acestream_${ACE_VERSION}.tar.gz |
-tar xzf -
-./acestreamengine --client-console --log-file acestream.log 2>&1 &
-cd -
+docker run --detach --name acestream-engine --publish 6878:6878 --env HOME=. \
+    vstavrinov/acestream-engine ./acestreamengine --client-console
+
 pip install --disable-pip-version-check --upgrade pip setuptools wheel
 pip install pytest-cov pytest-flake8 codecov
 
