6c20747773460a397df0055a119cc024cd6061b2
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2022-06-26 16:03:30 +0300

    use nginx proxy to overcome acestream restriction to localhost for testing

--- setup.sh
+++ setup.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-docker run --detach --name acestream-engine --publish 6878:6878 --env HOME=. \
-    vstavrinov/acestream-engine ./acestreamengine --client-console
+CMD='
+apk add nginx;
+echo "
+server {
+    listen       80;
+    location / {
+        proxy_pass http://localhost:6878;
+    }
+}
+" > /etc/nginx/http.d/default.conf;
+nginx;
+./acestreamengine --client-console;
+'
+
+docker run --detach --name acestream-engine --publish 6878:80 --env HOME=. \
+    vstavrinov/acestream-engine sh -c "$CMD"
 
 pip install --disable-pip-version-check --upgrade pip setuptools wheel
 pip install pytest-cov pytest-flake8 codecov
