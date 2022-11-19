0a36ba5417c40ca636ec32ecfd3ac9438e3a5baa
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2022-11-19 21:01:00 +0300

    upgrades and fixes:
    - upgrade checkout and setup-python
    - fix setup for updated engine

--- setup.sh
+++ setup.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 
-CMD='
-apk update;
-apk add nginx;
-echo "
-server {
-    listen       80;
-    location / {
-        proxy_pass http://localhost:6878;
-    }
-}
-" > /etc/nginx/http.d/default.conf;
-nginx;
-cd /srv/acestream
-./acestreamengine --client-console;
-'
-
-docker run --detach --name acestream-engine --publish 6878:80 --env HOME=. \
-    vstavrinov/acestream-engine sh -c "$CMD"
-
+docker run --name acestream-engine --detach --publish 6878:6878 \
+    vstavrinov/acestream-engine ./start-engine --client-console
 pip install --disable-pip-version-check --upgrade pip setuptools wheel
 pip install pytest-cov codecov flake8
 
