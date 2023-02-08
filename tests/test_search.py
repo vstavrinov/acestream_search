2f81e7b7acacbf738fe55bf44db29c1580948690
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2023-02-08 18:16:21 +0300

    fix tests:
    - add test_search.py to flake8 command line
    - remove extra import
    - remove unicode workaround for python2
    - adapt for URLs changes (getstream)
    - get back xml test

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,21 +1,13 @@
 # coding=utf8
 import json
 import re
-import sys
 import unittest
 
 from acestream_search.acestream_search import main, get_options
 
 channel = 'НТВ'
 m3u_re = re.compile('#EXTM3U#EXTINF:-1,[0-9]+\\. ' + channel +
-                    '.*\n.*/ace/manifest.m3u8\\?infohash=[0-9a-f]+')
-
-if sys.version_info[0] > 2:
-    def u_code(string):
-        return string
-else:
-    def u_code(string):
-        return string.encode("utf8")
+                    '.*\n.*/ace/getstream\\?infohash=[0-9a-f]+')
 
 
 def probe(args):
@@ -50,20 +42,20 @@ class TestQuery(unittest.TestCase):
         opts['show_epg'] = 1
         args = get_options(opts)
         self.assertIsNotNone(re.match('#EXTM3U#EXTINF:-1 tvg-id="[0-9]+",[0-9]+\\. ' + channel +
-                             '.*\n.*/ace/manifest.m3u8\\?infohash=[0-9a-f]+',
+                             '.*\n.*/ace/getstream\\?infohash=[0-9a-f]+',
                                       probe(args)))
 
-#    def test_xml(self):
-#        opts = {'query': channel}
-#        opts['xml_epg'] = 1
-#        args = get_options(opts)
-#        self.assertIsNotNone(re.search(' +<channel id="[0-9]+">\n +<display-name lang="ru">'
-#                             + channel, u_code(probe(args))))
+    def test_xml(self):
+        opts = {'query': channel}
+        opts['xml_epg'] = 1
+        args = get_options(opts)
+        self.assertIsNotNone(re.search(' +<channel id="[0-9]+">\n +<display-name lang="ru">'
+                             + channel, probe(args)))
 
     def test_json(self):
         opts = {'query': channel}
         opts['json'] = 1
         args = get_options(opts)
         item = json.loads(probe(args))[0]['items'][0]
-        self.assertTrue(channel in u_code(item['name']) and
+        self.assertTrue(channel in item['name'] and
                         re.match('[0-9a-f]+', item['infohash']))
