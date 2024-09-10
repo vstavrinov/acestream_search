220a2350409505dcc3f6814f0170ff681d6bc3be
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2024-09-10 19:32:05 +0300

    fix/update test:
    - rely on image CMD instead of docker run option
    - add 3.12 version to python matrix

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker run --name acestream-engine --detach --publish 6878:6878 \
-    vstavrinov/acestream-engine ./start-engine --client-console
+    vstavrinov/acestream-engine
 pip install --disable-pip-version-check --upgrade pip setuptools wheel
 pip install pytest-cov codecov flake8
 
